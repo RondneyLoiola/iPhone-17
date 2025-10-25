990836a368ee45a55b44fc820aaddb0599e99e18
RondneyLoiola <rondney.loiola11@gmail.com>
2025-10-24 22:12:14 -0300

    .

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,6 @@ function Intro() {
         { value: 'A18 Pro', label: 'Chip mais rápido', color: 'text-orange-500' },
         { value: '48MP', label: 'Sistema de câmera', color: 'text-blue-600' },
         { value: '29h', label: 'Bateria', color: 'text-orange-500' },
-
     ]
 
     return (
